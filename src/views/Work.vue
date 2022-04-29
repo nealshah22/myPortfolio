ef8b0a11ecf2bf2afd5a82a2a5c334001da47c9f
neal <nealshah04@gmail.com>
2022-04-28 23:51:43 -0400

    work updates

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -14,10 +14,10 @@
     <div class="column">
       <transition name="fade" appear>
         <div class="card">
-          <h3>Shift Manager | Kung Fu Tea</h3>
+          <h3>Shift Manager Promotion | Kung Fu Tea</h3>
           <img src="https://i.postimg.cc/j5sNFBD9/1200px-Kung-Fu-Tea-Official-Logo.png" alt="KFT" style="width:100%">
           <div class="container">
-            <h5>Shift manager</h5>
+            <h5>Shift Manager</h5>
             <p>
               <ul>
                 <li>Supervise up to 5 employees including baristas and kitchen staff</li>
